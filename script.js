98522967e05af9998136c735c4750e54d911f155
German Ramirez <german@sidewalk.ai>
2021-03-18 20:59:50 -0600

    Added variables and do a return of the password

--- script.js
+++ script.js
@@ -19,10 +19,16 @@ generateBtn.addEventListener("click", writePassword);
 
 
 function generatePassword() {
+  /*This variables will work for the criteria */
   var stringsLower = "abcdefghijklmnopqrstuvwxyz";
   var stringsUpper = stringsLower.toUpperCase();
   var numbers = '0123456789';
   var specialCharacters = '!”#$%&’()*+,-./:;<=>?@[\\]^_`{|}~';
-
+  /*  This varaibles will help me to create a logic*/
+  var password_temp = [];
+  var checkLength = true;
+  /* This variable will be the one that will contain the final password to present to the user */
+  var password = "";
+  return password;
 }
 
