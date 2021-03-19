d21b8b4e6bbf4d94051117e8f13bfdc1ab6bbce0
German Ramirez <german@sidewalk.ai>
2021-03-19 17:14:01 -0600

    Added the code that will create the random password, also I deleted some console.logs and I added comments on the code

--- script.js
+++ script.js
@@ -13,11 +13,6 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-// function generatePassword
-
-
-
 function generatePassword() {
   /*This variables will work for the criteria */
   var stringsLower = "abcdefghijklmnopqrstuvwxyz";
@@ -27,6 +22,10 @@ function generatePassword() {
   /*  This varaibles will help me to create a logic*/
   var password_temp = [];
   var checkLength = true;
+  var selectLower = "";
+  var selectUpper = "";
+  var selectNumber = "";
+  var selectSpecial = "";
   /* This variable will be the one that will contain the final password to present to the user */
   var password = "";
 
@@ -60,7 +59,7 @@ function generatePassword() {
     if (!confirm("Numbers!")) {
         hasNumber = false;
     }
-    //CHeck if any criteria is selected , if yes it outs of this loop if not it keep asking the user to select at least one criteria
+    //I  Check if any criteria is selected , if yes it outs of this loop if not it keep asking the user to select at least one criteria
     if (hasLower || hasUpper || hasSpecial || hasNumber) {
       checkConditions = false;
     }
@@ -68,7 +67,40 @@ function generatePassword() {
       alert('Please select at least one password criteria!');
     }
   }
+  // I create this loop in order to get the password according to the length 
+  while (password_temp.length < length) {
+    // This line id to get a random value of the string of the Lower Case letters
+    selectLower = stringsLower.charAt(Math.ceil(stringsLower.length * Math.random() * Math.random()));
+    // This line id to get a random value of the string of the Upper Case letters
+    selectUpper = stringsUpper.charAt(Math.ceil(stringsUpper.length * Math.random() * Math.random()));
+    // This line id to get a random value of the string of the Numbers
+    selectNumber = numbers.charAt(Math.ceil(numbers.length * Math.random() * Math.random()));
+    // This line id to get a random value of the string of the Special Character
+    selectSpecial = specialCharacters.charAt(Math.ceil(specialCharacters.length * Math.random()*Math.random()));
 
-  return password;
+    //V erify if on the criteria the user select that need an Lower Case Letter and if does I added to an array
+    if (hasLower) {
+      password_temp.push(selectLower);  
+    }
+    // Verify if on the criteria the user select that need an Upper Case Letter and if does I added to an array
+    if (hasUpper) {
+      password_temp.push(selectUpper);  
+    }
+    // Verify if on the criteria the user select that need a Special Character  and if does I added to an array
+    if (hasSpecial) {
+      password_temp.push(selectSpecial); 
+    }
+    // Verify if on the criteria the user select that need a Number and if does I added to an array
+    if (hasNumber) {
+      password_temp.push(selectNumber); 
+    }
+      
+  }
+  // I do the follow here , first I use sort and a function to create random sort of the values of the array
+  // the random order is using this (0.5 - Math.random()), second I conver the array into a string usign join
+  password = password_temp.sort(function () { return 0.5 - Math.random() }).join('');
+  // Here I make sure that the lenght is what the user set so if on the loop I added more elements I can extract those with the function substr
+  // I I return the value
+  return password.substr(0, length);
 }
 
